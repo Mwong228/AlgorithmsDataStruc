579651545e726982b827a369570ab85982180101
Michael Wong <michael.w228@gmail.com>
2021-04-06 17:10:36 -0400

    BigO for arrays

---
+++ BigO/arrays.js
@@ -0,0 +1,24 @@
+// Arrays - Ordered Lists
+
+let name = ["bob", "sam", "alex"]
+
+//BigO of Arrays
+
+// Insertion/Removal - depends
+//     add/remove at the beginning of array is O(n)
+//         need to re-index all elements
+//     add at the end of array is O(1)
+
+// Searching - O(n)
+
+// Access - O(1)
+
+
+
+//Array Methods
+// push/pop - O(1)
+// shift/unshift - O(n) -> Have to re-index all elements 
+// concat - O(n)
+// slice/splice - O(n)
+// *sort - O(N*logN)
+// *forEach/map/filter/reduce - O(n)
