ab7229149b8deb4e235d6b26f93a6495deb227e0
Michael Wong <michael.w228@gmail.com>
2021-04-21 13:55:27 -0400

    push function for DLL

---
+++ LinkedList/doublyLinkedList.js
@@ -0,0 +1,34 @@
+class Node {
+    constructor(value) {
+        this.value = value
+        this.next = null
+        this.prev = null
+    }
+}
+
+class DoublyLinkedList {
+    constructor() {
+        this.head = null
+        this.tail = null
+        this.length = 0
+    }
+    push(value) {
+        var newNode = new Node(value)
+        if (!this.head) {
+            this.head = newNode
+            this.tail = this.head
+        } else {
+            this.tail.next = newNode
+            newNode.prev = this.tail
+            this.tail = newNode
+        }
+        this.length++
+        return this
+    }
+
+}
+
+list = new DoublyLinkedList()
+
+list.push(99)
+list.push(105)
