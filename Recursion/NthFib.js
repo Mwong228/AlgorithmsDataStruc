fd47f90166db5955a514201846428a8572418b79
Michael Wong <michael.w228@gmail.com>
2021-04-20 22:19:33 -0400

    fib example for recursion

--- Recursion/NthFib.js
+++ Recursion/NthFib.js
@@ -1,3 +1,13 @@
+//time O(2^N)
+//space O(N) 
 function fibonacci(num){
-
+    if(num === 1){
+        return 0
+    }
+    if(num === 2){
+        return 1
+    }
+    else{
+        return fibonacci(num-1) + fibonacci(num-2)
+    }
 }
