d2b6c1dd9f4c8e9736f7395eb854e8d8e75ec313
Michael Wong <michael.w228@gmail.com>
2021-05-05 16:31:58 -0400

    updated hashTables

--- HashTables/hashTable.js
+++ HashTables/hashTable.js
@@ -1,10 +1,33 @@
-function hash(key, arrayLen){
-    let total = 0 
-    let primeNum = 31
-    for(let i = 0; i < Math.min(key.length, 100); i++){
-        let char = key[i]
-        let value = char.charCodeAt(0) - 96 
-        total = (total * primeNum + value) % arrayLen
+class HashTable{
+    constructor(size = 53){
+        this.keyMap = new Array(size)
     }
-    return total
+    hash(key){
+        let total = 0 
+        let primeNum = 31
+        for(let i = 0; i < Math.min(key.length, 100); i++){
+            let char = key[i]
+            let value = char.charCodeAt(0) - 96 
+            total = (total * primeNum + value) % this.keyMap.length
+        }
+        return total
+    }
+    set(key, value){
+        let index = this.hash(key)
+        if(!this.keyMap[index]){
+            this.keyMap[index] = []
+        }
+        this.keyMap[index].push([key, value])
+    }
+    get(key){
+        let index = this.hash(key)
+        if(this.keyMap[index]){
+            for(let i=0; i<this.keyMap[index].length; i++){
+                if(this.keyMap[index][i][0] === key){
+                    return this.keyMap[index][i][1]
+                }
+            }
+        }
+        return undefined
+    }
 }
