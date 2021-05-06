d715d805181268ad399aaeabb12464abc4b793b0
Michael Wong <michael.w228@gmail.com>
2021-05-05 20:30:55 -0400

    implemented DFS for graph

---
+++ Graph/DFS.js
@@ -0,0 +1,47 @@
+class Graph {
+    constructor() {
+        this.adjacencyList = {}
+    }
+    addVertex(vertex) {
+        if (!this.adjacencyList[vertex]) {
+            this.adjacencyList[vertex] = []
+        }
+    }
+    addEdge(v1, v2){
+        this.adjacencyList[v1].push(v2)
+        this.adjacencyList[v2].push(v1)
+    }
+    dfRecursive(start){
+        const result = []
+        const visited = {}
+        const adjacencyList = this.adjacencyList
+
+        function dfs(vertex){
+            if(!vertex) return null
+            visited[vertex] = true
+            result.push(vertex)
+            adjacencyList[vertex].forEach(next => {
+                if(!visited[next]) return dfs(next)
+            })
+        }
+        dfs(start)
+        return result
+
+    }
+
+}
+var g = new Graph ()
+g.addVertex("A")
+g.addVertex("B")
+g.addVertex("C")
+g.addVertex("D")
+g.addVertex("E")
+g.addVertex("F")
+
+g.addEdge("A","B")
+g.addEdge("A","C")
+g.addEdge("B","D")
+g.addEdge("C","E")
+g.addEdge("D","E")
+g.addEdge("D","F")
+g.addEdge("E","F")
